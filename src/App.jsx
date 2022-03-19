3ae98206d52dd0a873e0a5a27699e7c5c403938d
Jesus Rojas <jarojas6524@misena.edu.co>
2022-03-19 14:19:55 -0500

    layout success

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,62 @@
 import { useState } from 'react'
+import styled from '@emotion/styled'
+import ImagenCripto from './img/imagen-criptos.png'
+import Formulario from './components/Formulario'
+
+
+const Contenedor = styled.div`
+  max-width: 900px;
+  margin: 0 auto;
+  width: 90%;
+  @media(min-width: 992px) {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    column-gap: 2rem;
+  }
+`
+
+const Imagen = styled.img`
+  max-width: 400px;
+  margin: 100px auto 0 auto;
+  width: 80%;
+  display: block;
+`
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #FFF;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, asperiores fuga repudiandae rerum omnis expedita nulla non, sit doloribus aut aperiam voluptatum repellendus ipsum? Enim nobis velit eos earum eveniet!
-      Eligendi numquam suscipit, optio debitis quaerat, illum facilis qui recusandae dicta a non voluptatem accusantium esse harum ea quam cum ipsa explicabo. Id et ad cum, qui at laborum commodi!
-      Officia earum tempore, architecto delectus iusto necessitatibus reiciendis labore eveniet in dolor suscipit neque voluptatibus molestias eligendi ratione cum reprehenderit veritatis laudantium velit maiores impedit temporibus nobis! Ut, eum tenetur.
-      Tenetur deserunt dignissimos officia cumque porro magni reprehenderit beatae necessitatibus doloremque tempore praesentium modi excepturi recusandae libero a et, voluptatum, aperiam sequi, ut culpa odit velit. Aspernatur provident perferendis a!
-      Libero repudiandae quia fugiat fugit labore accusantium sunt. Eveniet quam earum adipisci modi, architecto ab voluptatem impedit iure. Unde nulla qui assumenda debitis maxime ipsa fugit quam vel. Assumenda, tenetur?
-      Eligendi qui veniam alias dolores, accusamus et quo, harum quam exercitationem temporibus similique molestiae omnis consequatur repellat. Doloribus vero nesciunt molestias nulla ducimus voluptates illo odit possimus! Corporis, quaerat explicabo!
-      Beatae, soluta! Possimus quos odit placeat dolorum dolores aliquam excepturi eum saepe iusto est nihil doloribus ut labore, tempore exercitationem rem necessitatibus at. Eaque repudiandae, quas ipsum voluptatibus maiores at.
-      Fuga cumque nostrum alias voluptatum. Reiciendis quidem cupiditate consequuntur tenetur voluptatibus rerum. Aliquid incidunt suscipit quo magni adipisci. Laboriosam, aperiam eveniet doloribus labore quam quaerat necessitatibus eligendi. Cumque, beatae labore!
-      Aperiam, nam fugiat veniam possimus non distinctio repellat neque accusamus quaerat eum laudantium aliquam a aut rerum aspernatur! Sint deleniti ducimus facere veniam omnis ipsa at iste praesentium, facilis fuga?
-      Odit quod, alias mollitia corrupti accusantium voluptatum dolores perferendis optio eveniet unde culpa ea autem dicta adipisci molestiae minima iste cum et quibusdam dolorum beatae quidem quo? Asperiores, iure sit.
-    </>
+    <Contenedor>
+      <Imagen 
+        src={ImagenCripto}
+        alt='Imagen Criptomonedas'
+      />
+      <div>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+        <Formulario 
+          
+        />
+      </div>
+    </Contenedor>
   )
 }
 
